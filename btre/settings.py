7087cf508be0501b2a24c26add92d66b60fecb5a
Maxwell Muhanda <ajaybullec@gmail.com>
2021-09-06 10:45:43 +0300

    removed email

--- btre/settings.py
+++ btre/settings.py
@@ -15,9 +15,6 @@ import json
 
 
 
-with open("/etc/config.json") as config_file:
-    config = json.load(config_file)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.get('DJANGO_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -152,17 +149,6 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
-
-# Email config
-
-
-
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = config.get('MAIL_USERNAME')
-EMAIL_HOST_PASSWORD = config.get('MAIL_PASSWORD')
-EMAIL_USE_TLS =True
 
 
 try:
